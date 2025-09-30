a0890960856c39c494517f39aff355f1a5041ee8
Michael Sammler <noreply@sammler.me>
2025-09-30 11:09:48 +0200

    Relax Hint modes from !640

--- stdpp/base.v
+++ stdpp/base.v
@@ -359,12 +359,15 @@ Global Instance equiv_default_relation `{Equiv A} :
 Global Hint Extern 0 (_ ≡ _) => reflexivity : core.
 Global Hint Extern 0 (_ ≡ _) => symmetry; assumption : core.
 
-(** These [Hint Mode] declarations are missing in the stdlib *)
-Global Hint Mode Reflexive ! ! : typeclass_instances.
-Global Hint Mode Symmetric ! ! : typeclass_instances.
-Global Hint Mode Transitive ! ! : typeclass_instances.
-Global Hint Mode PreOrder ! ! : typeclass_instances.
-Global Hint Mode Equivalence ! ! : typeclass_instances.
+(** These [Hint Mode] declarations are missing in the stdlib. These
+hint modes prevent instantiating [?R] with an arbitrary relation when
+searching for [@Reflexive ?A ?R], but allow finding an instance for
+[@Reflexive ?A (@eq ?A)]. *)
+Global Hint Mode Reflexive - ! : typeclass_instances.
+Global Hint Mode Symmetric - ! : typeclass_instances.
+Global Hint Mode Transitive - ! : typeclass_instances.
+Global Hint Mode PreOrder - ! : typeclass_instances.
+Global Hint Mode Equivalence - ! : typeclass_instances.
 
 (** * Type classes *)
 (** ** Decidable propositions *)
