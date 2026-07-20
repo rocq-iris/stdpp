ba0383ef23b1cde12d26d4ba6ba617749d831962
Thomas Somers <13411-tlsomers@users.noreply.gitlab.mpi-sws.org>
2026-07-20 15:18:39 +0000

    Reorder telescope file and remove local coersion of `tele_app`

--- stdpp/telescopes.v
+++ stdpp/telescopes.v
@@ -16,29 +16,7 @@ Inductive tele : Type :=
 
 Global Arguments TeleS {_} _.
 
-(** The telescope version of Rocq's function type *)
-Fixpoint tele_fun (TT : tele) (T : Type) : Type :=
-  match TT with
-  | TeleO => T
-  | TeleS b => ∀ x, tele_fun (b x) T
-  end.
-
-Notation "TT -t> A" :=
-  (tele_fun TT A) (at level 99, A at level 200, right associativity).
-
-(** An eliminator for elements of [tele_fun].
-    We use a [fix] because, for some reason, that makes stuff print nicer
-    in the proofs in iris:bi/lib/telescopes.v *)
-Definition tele_fold {X Y} {TT : tele} (step : ∀ {A : Type}, (A → Y) → Y) (base : X → Y)
-  : (TT -t> X) → Y :=
-  (fix rec {TT} : (TT -t> X) → Y :=
-     match TT as TT return (TT -t> X) → Y with
-     | TeleO => λ x : X, base x
-     | TeleS b => λ f, step (λ x, rec (f x))
-     end) TT.
-Global Arguments tele_fold {_ _ !_} _ _ _ /.
-
-(** A duplication of the type [sigT] to avoid any connection to other universes
+(** A duplication of the type [sigT] to avoid any connection to other universes.
  *)
 Record tele_arg_cons {X : Type} (f : X → Type) : Type := TeleArgCons
   { tele_arg_head : X;
@@ -79,6 +57,28 @@ Proof.
   - intros [x xs]. by apply HS.
 Qed.
 
+(** The telescope version of Rocq's function type *)
+Fixpoint tele_fun (TT : tele) (T : Type) : Type :=
+  match TT with
+  | TeleO => T
+  | TeleS b => ∀ x, tele_fun (b x) T
+  end.
+
+Notation "TT -t> A" :=
+  (tele_fun TT A) (at level 99, A at level 200, right associativity).
+
+(** An eliminator for elements of [tele_fun].
+    We use a [fix] because, for some reason, that makes stuff print nicer
+    in the proofs in iris:bi/lib/telescopes.v *)
+Definition tele_fold {X Y} {TT : tele} (step : ∀ {A : Type}, (A → Y) → Y) (base : X → Y)
+  : (TT -t> X) → Y :=
+  (fix rec {TT} : (TT -t> X) → Y :=
+     match TT as TT return (TT -t> X) → Y with
+     | TeleO => λ x : X, base x
+     | TeleS b => λ f, step (λ x, rec (f x))
+     end) TT.
+Global Arguments tele_fold {_ _ !_} _ _ _ /.
+
 Fixpoint tele_app {TT : tele} {U} : (TT -t> U) -> TT → U :=
   match TT as TT return (TT -t> U) -> TT → U with
   | TeleO => λ F _, F
@@ -88,9 +88,6 @@ Fixpoint tele_app {TT : tele} {U} : (TT -t> U) -> TT → U :=
 (* The bidirectionality hint [&] simplifies defining tele_app-based notation
 such as the atomic updates and atomic triples in Iris. *)
 Global Arguments tele_app {!_ _} & _ !_ /.
-
-(* This is a local coercion because otherwise, the "λ.." notation stops working. *)
-Local Coercion tele_app : tele_fun >-> Funclass.
 
 (** Inversion lemma for [tele_arg] *)
 Lemma tele_arg_inv {TT : tele} (a : tele_arg TT) :
@@ -106,16 +103,16 @@ Lemma tele_arg_S_inv {X} {f : X → tele} (a : TeleS f) :
 Proof. exact (tele_arg_inv a). Qed.
 
 (** Map below a tele_fun *)
-Fixpoint tele_map {T U} {TT : tele} : (T → U) → (TT -t> T) → TT -t> U :=
+Fixpoint tele_map {TT : tele} {T U} : (T → U) → (TT -t> T) → TT -t> U :=
   match TT as TT return (T → U) → (TT -t> T) → TT -t> U with
   | TeleO => λ F : T → U, F
   | @TeleS X b => λ (F : T → U) (f : TeleS b -t> T) (x : X),
                   tele_map F (f x)
   end.
-Global Arguments tele_map {_ _ !_} _ _ /.
+Global Arguments tele_map {!_ _ _} _ _ /.
 
-Lemma tele_map_app {T U} {TT : tele} (F : T → U) (t : TT -t> T) (x : TT) :
-  (tele_map F t) x = F (t x).
+Lemma tele_map_app {TT : tele} {T U} (F : T → U) (t : TT -t> T) (x : TT) :
+  tele_app (tele_map F t) x = F (tele_app t x).
 Proof.
   induction TT as [|X f IH]; simpl in *.
   - rewrite (tele_arg_O_inv x). done.
@@ -126,21 +123,21 @@ Qed.
 Global Instance tele_fmap {TT : tele} : FMap (tele_fun TT) := λ T U, tele_map.
 
 Lemma tele_fmap_app {T U} {TT : tele} (F : T → U) (t : TT -t> T) (x : TT) :
-  (F <$> t) x = F (t x).
+  tele_app (F <$> t) x = F (tele_app t x).
 Proof. apply tele_map_app. Qed.
 
 (** Operate below [tele_fun]s with argument telescope [TT]. *)
-Fixpoint tele_bind {U} {TT : tele} : (TT → U) → TT -t> U :=
+Fixpoint tele_bind {TT : tele} {U} : (TT → U) → TT -t> U :=
   match TT as TT return (TT → U) → TT -t> U with
   | TeleO => λ F, F tt
   | @TeleS X b => λ (F : TeleS b → U) (x : X), (* b x -t> U *)
       tele_bind (λ a, F (TargS x a))
   end.
-Global Arguments tele_bind {_ !_} _ /.
+Global Arguments tele_bind {!_ _} _ /.
 
 (* Show that tele_app ∘ tele_bind is the identity. *)
-Lemma tele_app_bind {U} {TT : tele} (f : TT → U) x :
-  (tele_bind f) x = f x.
+Lemma tele_app_bind {TT : tele} {U} (f : TT → U) x :
+  tele_app (tele_bind f) x = f x.
 Proof.
   induction TT as [|X b IH]; simpl in *.
   - rewrite (tele_arg_O_inv x). done.
@@ -153,7 +150,7 @@ space. *)
 Definition tele_fun_id {TT : tele} : TT -t> TT := tele_bind id.
 
 Lemma tele_fun_id_eq {TT : tele} (x : TT) :
-  tele_fun_id x = x.
+  tele_app tele_fun_id x = x.
 Proof. unfold tele_fun_id. rewrite tele_app_bind. done. Qed.
 
 Definition tele_fun_compose {TT1 TT2 TT3 : tele} :
@@ -161,7 +158,7 @@ Definition tele_fun_compose {TT1 TT2 TT3 : tele} :
   λ t1 t2, tele_bind (compose (tele_app t1) (tele_app t2)).
 
 Lemma tele_fun_compose_eq {TT1 TT2 TT3 : tele} (f : TT2 -t> TT3) (g : TT1 -t> TT2) x :
-  tele_fun_compose f g $ x = (f ∘ g) x.
+  tele_app (tele_fun_compose f g) x = (tele_app f ∘ tele_app g) x.
 Proof. unfold tele_fun_compose. rewrite tele_app_bind. done. Qed.
 
 (** Notation *)
